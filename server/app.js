3b8e21eef0ff0e9c15ba63c16d1471fa030d5195
Rohan Lekhwani <rohanlekhwani@ymail.com>
2021-01-25 17:33:07 +0530

    Merge develop into create_channel

--- server/app.js
+++ server/app.js
@@ -4,11 +4,13 @@ const passport = require('passport')
 const jwtStrategy = require('./config/passport-jwt-strategy')
 const db = require('./config/mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const port = process.env.PORT || 3030
 
 app.use(bodyParser.json())
 app.use(passport.initialize())
+app.use(cors());
 
 app.use('/', require('./routes'))
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const { Router } = require("express")
 const router = Router()
 const loginController = require('../controllers/login')
+const getRepositoriesController = require('../controllers/getRepositories')
+const createChannelController = require('../controllers/createChannel')
 
 router.get('/auth/github/callback', loginController.createToken)
+router.get('/getRepositories', getRepositoriesController);
+router.post('/createChannel', createChannelController)
 
 module.exports = router;
